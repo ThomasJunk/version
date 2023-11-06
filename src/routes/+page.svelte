8590aafa76a4ee1dc8275455b13c15d656f4bd7b
Thomas Junk <thomas.junk@intevation.de>
2023-11-06 17:14:10 +0100

    Display of version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const version = __APP_VERSION__;
+</script>
+
+<h1>Welcome to MyApp {version}</h1>
